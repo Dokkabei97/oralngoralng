19b2d923d4a877a13fbe036c1aba4443ed71173b
Dokkabei97 <wkdrn970@naver.com>
2022-05-03 23:47:34 +0900

    💄: 검색 필드 변경

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
 import {styled, alpha} from '@mui/material/styles';
 import Box from "@mui/material/Box";
-import {Grid, InputBase, TextField} from "@mui/material";
+import {Grid, InputBase, OutlinedInputProps, TextField, TextFieldProps} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import InputAdornment from '@mui/material/InputAdornment';
 
-const CustomTextField = styled(TextField)({
-    '& label.Mui-focused': {
-        color: 'black',
-    },
-    '& .MuiInput-underline:after': {
-        borderBottomColor: 'black',
-    },
-    '& .MuiOutlinedInput-root': {
-        '& fieldset': {
-            borderColor: 'black',
-        },
-        '&:hover fieldset': {
-            borderColor: 'black',
-        },
-        '&.Mui-focused fieldset': {
-            borderColor: 'black',
-        },
+const CustomTextField = styled((props: TextFieldProps) => (
+    <TextField
+        InputProps={{ disableUnderline: true } as Partial<OutlinedInputProps>}
+        {...props}
+    />
+    ))(({ theme}) => ({
+    '& .MuiFilledInput-root': {
+        borderRadius: 4,
+        backgroundColor: 'white',
     },
     '& .MuiInputBase-root': {
         backgroundColor: 'white',
     }
-});
+}));
 
 const SearchBar = () => {
     return (
@@ -36,8 +28,9 @@ const SearchBar = () => {
                     <Box m={4}>
                         <CustomTextField
                             fullWidth
-                            variant={"outlined"}
-                            // label={"여행지를 검색해 보세요"}
+                            variant={"filled"}
+                            label={"여행지를 검색해 보세요"}
+
                             InputProps={{
                                 startAdornment: (
                                     <InputAdornment position="start">
