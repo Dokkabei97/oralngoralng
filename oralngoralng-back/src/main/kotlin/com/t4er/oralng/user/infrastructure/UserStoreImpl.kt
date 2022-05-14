f2684be23e7a179a0921042d9b77f2d565b38ca1
Dokkabei97 <wkdrn970@naver.com>
2022-05-15 03:51:37 +0900

    test: controller test 추가 #2
    feat: controller 추가 #2
    
    feat: security 추가

--- oralngoralng-back/src/main/kotlin/com/t4er/oralng/user/infrastructure/UserStoreImpl.kt
+++ oralngoralng-back/src/main/kotlin/com/t4er/oralng/user/infrastructure/UserStoreImpl.kt
@@ -8,12 +8,7 @@ import org.springframework.stereotype.Component
 class UserStoreImpl(val userRepository: UserRepository): UserStore {
 
     override fun store(user: User): User {
-        validCheck(user)
         return userRepository.save(user)
     }
 
-    // TODO: 2022-05-15 예외 정의
-    fun validCheck(user: User) {
-        if (user.userToken == null) throw RuntimeException()
-    }
 }
---
+++ oralngoralng-back/src/main/kotlin/com/t4er/oralng/user/ui/UserDto.kt
@@ -0,0 +1,21 @@
+package com.t4er.oralng.user.ui
+
+import com.t4er.oralng.user.domain.UserCommand
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+
+class UserDto {
+
+    data class CreateUserDto(
+        @NotBlank(message = "닉네임은 필수입니다.")
+        var nickname: String,
+        @Email(message = "이메일 형식과 다릅니다.")
+        var email: String
+    ) {
+        companion object {
+            fun toCommand(nickname: String, email: String): UserCommand.RegisterUserRequest {
+                return UserCommand.RegisterUserRequest(nickname, email)
+            }
+        }
+    }
+}
