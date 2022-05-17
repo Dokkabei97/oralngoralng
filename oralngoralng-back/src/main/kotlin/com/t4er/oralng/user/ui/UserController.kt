a505954076f6fa99bafc83229edaa5b67119395d
Dokkabei97 <wkdrn970@naver.com>
2022-05-18 00:10:05 +0900

    feat: Controller Test 테스트 값 추가 #16
    feat: Controller 반환 값 추가 #16

--- oralngoralng-back/src/main/kotlin/com/t4er/oralng/user/ui/UserController.kt
+++ oralngoralng-back/src/main/kotlin/com/t4er/oralng/user/ui/UserController.kt
@@ -1,6 +1,8 @@
 package com.t4er.oralng.user.ui
 
 import com.t4er.oralng.user.application.UserFacade
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,10 +14,11 @@ import javax.validation.Valid
 class UserController(val userFacade: UserFacade) {
 
     @PostMapping
-    fun createUser(@RequestBody @Valid dto: UserDto.CreateUserDto) {
-        val toCommand = UserDto.CreateUserDto.toCommand(dto.nickname, dto.email)
-        userFacade.createUser(toCommand)
+    fun createUser(@RequestBody @Valid dto: UserDto.CreateUserRequest) : ResponseEntity<UserDto.CreateUserResponse> {
+        val toCommand = UserDto.CreateUserRequest.toCommand(dto.nickname, dto.email)
+        val createUser = userFacade.createUser(toCommand)
 
-        // TODO: 2022-05-15 반환 값 추가 후 테스트 코드 추가 검증 필요
+        return ResponseEntity<UserDto.CreateUserResponse>(UserDto.CreateUserResponse(createUser.nickname, createUser.email), HttpStatus.OK)
+        // TODO: 2022-05-18 반환 값 변경 필요
     }
 }
--- oralngoralng-back/src/test/kotlin/com/t4er/oralng/user/ui/UserControllerTest.kt
+++ oralngoralng-back/src/test/kotlin/com/t4er/oralng/user/ui/UserControllerTest.kt
@@ -1,19 +1,16 @@
 package com.t4er.oralng.user.ui
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.t4er.oralng.user.application.UserFacade
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import java.nio.charset.StandardCharsets
 
 @SpringBootTest
@@ -26,10 +23,10 @@ class UserControllerTest(
         const val URI = "/api/v1/users"
     }
 
-    @DisplayName("유저 생성 api")
+    @DisplayName("유저 생성 api 성공")
     @Test
-    fun create_user() {
-        val dto: UserDto.CreateUserDto = UserDto.CreateUserDto("닉네임", "test@test.com")
+    fun create_user_success() {
+        val dto: UserDto.CreateUserRequest = UserDto.CreateUserRequest("닉네임", "test@test.com")
 
         val requestBuilder = MockMvcRequestBuilders.post(URI)
             .content(objectMapper.writeValueAsString(dto))
@@ -38,8 +35,10 @@ class UserControllerTest(
             .characterEncoding(StandardCharsets.UTF_8.displayName())
 
         mockMvc.perform(requestBuilder)
-            .andExpect(MockMvcResultMatchers.status().isOk)
-
-        // TODO: 2022-05-15 테스트 코드 더 검증 필요
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("\$.nickname").value("닉네임"))
+            .andExpect(jsonPath("\$.email").value("test@test.com"))
     }
+
+
 }
