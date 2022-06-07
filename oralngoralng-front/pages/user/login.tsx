625e0ae726ef0bac4629f9db8f6b5c70358b8673
Dokkabei97 <wkdrn970@naver.com>
2022-06-07 23:16:20 +0900

    feat: 로그인 페이지

---
+++ oralngoralng-front/pages/user/login.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from "../../components/layout";
+import {Grid} from "@mui/material";
+
+const Login = () => {
+    return (
+        <Layout>
+            <Grid>
+                Login 화면
+            </Grid>
+        </Layout>
+    );
+};
+
+export default Login;
