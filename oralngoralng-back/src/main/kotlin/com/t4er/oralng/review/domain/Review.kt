ff3c7131a514df051b822490565827c86b67eae7
Dokkabei97 <wkdrn970@naver.com>
2022-06-21 23:48:04 +0900

    feat: 리뷰 기능 개발#5

--- oralngoralng-back/src/main/kotlin/com/t4er/oralng/review/domain/Review.kt
+++ oralngoralng-back/src/main/kotlin/com/t4er/oralng/review/domain/Review.kt
@@ -1,6 +1,8 @@
 package com.t4er.oralng.review.domain
 
 import com.t4er.oralng.common.entity.AbstractEntity
+import com.t4er.oralng.common.exception.InvalidParamException
+import com.t4er.oralng.user.domain.User
 import javax.persistence.*
 
 @Entity
@@ -9,8 +11,28 @@ class Review(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_id")
-    var id: Long?
+    var id: Long?,
 
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    var user: User,
+
+    @Column(name = "title")
+    var title: String,
+
+    @Column(name = "content", columnDefinition = "text")
+    var content: String,
+
+    @Column(name = "hit_count")
+    var hitCount: Int,
+
+    @Column(name = "likes_count")
+    var likeCount: Int,
 ): AbstractEntity() {
 
+    companion object {
+        fun of(user: User, title: String, content: String): Review {
+            return Review(null, user, title, content, 0, 0)
+        }
+    }
 }
