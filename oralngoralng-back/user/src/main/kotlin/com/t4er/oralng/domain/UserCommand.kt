e16fc4de9e4172f30af1e671074cbf0725fbc494
Dokkabei97 <wkdrn970@naver.com>
2022-09-13 22:29:35 +0900

    feat: valid 수정

--- oralngoralng-back/user/src/main/kotlin/com/t4er/oralng/domain/UserCommand.kt
+++ oralngoralng-back/user/src/main/kotlin/com/t4er/oralng/domain/UserCommand.kt
@@ -1,12 +1,34 @@
 package com.t4er.oralng.domain
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
 class UserCommand {
 
-    class RegisterUserRequest(val nickname: String, val email: String)
+    class RegisterUserRequest(
+        @field: NotBlank(message = "nickname 은 필수 입니다.")
+        val nickname: String,
 
-    class UpdateUserRequest(val userId: Long, val nickname: String)
+        @field: NotBlank(message = "email 은 필수 입니다.")
+        @field: Email(message = "이메일 형식을 맞춰 주세요.")
+        val email: String,
+    )
 
-    class DeleteUserRequest(val userId: Long)
+    class UpdateUserRequest(
+        @field: NotNull(message = "userId 는 필수 입니다.")
+        val userId: Long,
+        @field: NotBlank(message = "nickname 은 필수 입니다.")
+        val nickname: String,
+    )
 
-    class GetUserRequest(val userId: Long)
+    class DeleteUserRequest(
+        @field: NotNull(message = "userId 는 필수 입니다.")
+        val userId: Long,
+    )
+
+    class GetUserRequest(
+        @field: NotNull(message = "userId 는 필수 입니다.")
+        val userId: Long,
+    )
 }
--- oralngoralng-back/user/src/main/kotlin/com/t4er/oralng/interface/UserController.kt
+++ oralngoralng-back/user/src/main/kotlin/com/t4er/oralng/interface/UserController.kt
@@ -1,16 +1,14 @@
 package com.t4er.oralng.`interface`
 
-import org.slf4j.LoggerFactory
 import com.t4er.oralng.application.UserFacade
-import com.t4er.oralng.domain.UserCommand
 import com.t4er.oralng.domain.UserCommand.*
 import com.t4er.oralng.domain.UserInfo
+import org.slf4j.LoggerFactory
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
-import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
+import javax.validation.Valid
 
 @Controller
 class UserController(val userFacade: UserFacade) {
@@ -18,22 +16,22 @@ class UserController(val userFacade: UserFacade) {
     private val log = LoggerFactory.getLogger(UserController::class.java)
 
     @MutationMapping
-    fun registerUser(@Argument @Validated input: RegisterUserRequest): UserInfo {
+    fun registerUser(@Argument @Valid input: RegisterUserRequest): UserInfo {
         return userFacade.registerUser(input)
     }
 
     @MutationMapping
-    fun updateUser(@Argument @Validated input: UpdateUserRequest): UserInfo {
+    fun updateUser(@Argument @Valid input: UpdateUserRequest): UserInfo {
         return userFacade.updateUser(input)
     }
 
     @MutationMapping
-    fun deleteUser(@Argument @Validated input: DeleteUserRequest) {
+    fun deleteUser(@Argument @Valid input: DeleteUserRequest) {
         userFacade.deleteUser(input)
     }
 
     @QueryMapping
-    fun getUser(@Argument @Validated input: GetUserRequest): UserInfo {
+    fun getUser(@Argument @Valid input: GetUserRequest): UserInfo {
         return userFacade.getUser(input)
     }
 }
