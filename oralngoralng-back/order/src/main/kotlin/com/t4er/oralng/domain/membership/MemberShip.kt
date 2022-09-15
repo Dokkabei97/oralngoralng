64456d37689c9aefe36b1a9ecef35f6fa4e918c6
Dokkabei97 <wkdrn970@naver.com>
2022-09-15 22:40:39 +0900

    feat: 멤버쉽, 결제 타입 구현

---
+++ oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/domain/membership/MemberShip.kt
@@ -0,0 +1,28 @@
+package com.t4er.oralng.domain.membership
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "memberships")
+class MemberShip(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "membership_id")
+    var id: Long?,
+
+    @Column(name = "membership_name")
+    var name: MemberShipName,
+
+    @Column(name = "membership_description")
+    var description: String,
+
+    @Column(name = "membership_price")
+    var price: Int,
+) {
+    enum class MemberShipName(description: String, price: Int) {
+        BASIC("베이직 등급 ...", 5000),
+        STANDARD("스탠다드 등급, ...", 7500),
+        PREMIUM("프리미엄 등급, ...", 10000)
+    }
+}
