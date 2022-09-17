c6099bcffb50394a3ce592b2f8af753549ce37ea
Dokkabei97 <wkdrn970@naver.com>
2022-09-18 02:52:31 +0900

    test: order 테스트

--- oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/domain/order/Order.kt
+++ oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/domain/order/Order.kt
@@ -10,55 +10,37 @@ import javax.persistence.*
 @Entity
 @Table(name = "orders")
 class Order(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "order_id")
-    var id: Long?,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "order_id") var id: Long?,
 
-    @Column(name = "order_token")
-    var orderToken: String,
+    @Column(name = "order_token") var orderToken: String,
 
-    @Column(name = "user_id")
-    var userId: Long,
+    @Column(name = "user_id") var userId: Long,
 
-    @Column(name = "product_id")
-    var productId: Long,
+    @Column(name = "product_id") var productId: Long,
 
-    @Column(name = "card_id")
-    var cardId: Long?,
+    @Column(name = "card_id") var cardId: Long?,
 
-    @Column(name = "price")
-    var price: Int,
+    @Column(name = "price") var price: Int,
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "pay_type")
-    var payType: PayMethod?,
+    @Enumerated(EnumType.STRING) @Column(name = "pay_type") var payType: PayMethod?,
 
-    @Column(name = "billing_key")
-    var billingKey: String?,
+    @Column(name = "billing_key") var billingKey: String?,
 
-    @Column(name = "order_at")
-    var orderAt: ZonedDateTime,
+    @Column(name = "order_at") var orderAt: ZonedDateTime,
 
-    @Column(name = "expiration_product_at")
-    var expirationProductAt: ZonedDateTime,
+    @Column(name = "expiration_product_at") var expirationProductAt: ZonedDateTime?,
 
-    @Column(name = "next_payment_at")
-    var nextPaymentAt: ZonedDateTime,
+    @Column(name = "next_payment_at") var nextPaymentAt: ZonedDateTime?,
 
-    @Embedded
-    var commonEntity: CommonEntity?
+    @Embedded var commonEntity: CommonEntity?,
 
-) : AbstractEntity() {
+    ) : AbstractEntity() {
     companion object {
         private const val ORDER_PREFIX: String = "ord_"
         fun of(
             userId: Long,
             productId: Long,
             price: Int,
-            orderAt: ZonedDateTime,
-            expirationAt: ZonedDateTime,
-            nextOrderAt: ZonedDateTime,
         ): Order {
             return Order(
                 null,
@@ -69,19 +51,11 @@ class Order(
                 price,
                 null,
                 null,
-                orderAt,
-                expirationAt,
-                nextOrderAt,
+                ZonedDateTime.now(),
+                null,
+                null,
                 null
             )
         }
     }
-}
-
-private fun isValidExpirationDate(): Boolean {
-    return false
-}
-
-private fun isValidNextPaymentDate(): Boolean {
-    return false
 }
--- oralngoralng-back/order/src/test/kotlin/com/t4er/oralng/domain/order/OrderTest.kt
+++ oralngoralng-back/order/src/test/kotlin/com/t4er/oralng/domain/order/OrderTest.kt
@@ -3,6 +3,7 @@ package com.t4er.oralng.domain.order
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldStartWith
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -18,7 +19,23 @@ internal class OrderTest : DescribeSpec({
                 .withSecond(0)
                 .withNano(0)
             it("성공") {
-                expirateAt shouldBe ZonedDateTime.of(now.year, now.monthValue.plus(3), now.dayOfMonth, 12, 0, 0, 0, ZoneId.of("Asia/Seoul"))
+                expirateAt shouldBe ZonedDateTime.of(
+                    now.year,
+                    now.monthValue.plus(3),
+                    now.dayOfMonth,
+                    12,
+                    0,
+                    0,
+                    0,
+                    ZoneId.of("Asia/Seoul")
+                )
+            }
+        }
+        context("주문 생성") {
+            val order:Order = Order.of(1L, 1L, 5000)
+            it("성공") {
+                order.userId shouldBe  1L
+                order.orderToken shouldStartWith "ord_"
             }
         }
     }
