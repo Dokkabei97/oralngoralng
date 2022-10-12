980a28cd768896f48b3f667046ef6a39745ae1f5
Dokkabei97 <wkdrn970@naver.com>
2022-10-12 09:37:01 +0900

    feat: auth 페이지 분리

--- oralngoralng-admin-front/pages/auth/index.tsx
+++ oralngoralng-admin-front/pages/auth/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Auth from "../components/auth/Auth";
+import Auth from "../../components/auth/Auth";
 import {Button, Grid, TextField} from '@mui/material';
-import styles from 'pages/auth/_auth_page.module.scss';
+import styles from './_auth_page.module.scss';
 
 const Index = () => {
     return (
         <Auth>
-            <Box className={styles.box} xs={12}>
+            <Box className={styles.box}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <Typography className={styles.typography}>ORALNG ORALNG</Typography>
