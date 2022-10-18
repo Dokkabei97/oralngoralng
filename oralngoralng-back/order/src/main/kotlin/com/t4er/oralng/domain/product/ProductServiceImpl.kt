db61cbc971c10f7e5f219af9fe79ff5ba56d98f8
Dokkabei97 <wkdrn970@naver.com>
2022-10-18 22:59:13 +0900

    feat: product service 개발 #8

--- oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/domain/product/ProductServiceImpl.kt
+++ oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/domain/product/ProductServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.t4er.oralng.domain.product
 
+import com.t4er.oralng.domain.product.ProductCommand.*
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -7,5 +8,16 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class ProductServiceImpl(val productStore: ProductStore, val productReader: ProductReader): ProductService {
 
-
+    @Transactional
+    override fun registerProduct(command: RegisterProductRequest): ProductInfo {
+        val product = productStore.store(
+            Product.of(
+                command.membershipName,
+                command.name,
+                command.price,
+                command.duration
+            )
+        )
+        return ProductInfo(product)
+    }
 }
