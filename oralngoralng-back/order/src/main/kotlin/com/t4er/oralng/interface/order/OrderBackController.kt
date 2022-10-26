734a34fd7a9c7bc1b65e7beee948770798700dfb
Dokkabei97 <wkdrn970@naver.com>
2022-10-27 07:58:45 +0900

    feat: 컨트롤러 수정

--- oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/interface/order/OrderBackController.kt
+++ oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/interface/order/OrderBackController.kt
@@ -3,6 +3,7 @@ package com.t4er.oralng.`interface`.order
 import com.t4er.oralng.application.order.OrderFacade
 import com.t4er.oralng.domain.order.OrderCommand.*
 import com.t4er.oralng.domain.order.OrderInfo
+import com.t4er.oralng.response.CommonHttpResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController
 class OrderBackController(val orderFacade: OrderFacade) {
 
     @GetMapping("/{userId}")
-    fun getOrders(@PathVariable userId: Long): List<OrderInfo> {
-        return orderFacade.getOrders(GetOrdersRequest(userId))
+    fun getOrders(@PathVariable userId: Long): CommonHttpResponse<List<OrderInfo>> {
+        val orders = orderFacade.getOrders(GetOrdersRequest(userId))
+        return CommonHttpResponse.success(orders)
     }
 }
--- oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/interface/product/ProductController.kt
+++ oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/interface/product/ProductController.kt
@@ -1,19 +1,10 @@
 package com.t4er.oralng.`interface`.product
 
 import com.t4er.oralng.application.product.ProductFacade
-import com.t4er.oralng.domain.product.ProductCommand
 import com.t4er.oralng.domain.product.ProductCommand.*
 import com.t4er.oralng.domain.product.ProductInfo
 import com.t4er.oralng.response.CommonHttpResponse
-import org.springframework.graphql.data.method.annotation.MutationMapping
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
