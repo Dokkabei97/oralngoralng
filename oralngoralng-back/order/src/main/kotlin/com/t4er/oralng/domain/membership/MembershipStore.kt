0de263d200a5d7af4121075ad105611b7eeff9b2
Dokkabei97 <wkdrn970@naver.com>
2022-11-01 19:08:40 +0900

    feat: membership 삭제

--- oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/domain/membership/MembershipStore.kt
+++
@@ -1,4 +0,0 @@
-package com.t4er.oralng.domain.membership
-
-interface MembershipStore {
-}
--- oralngoralng-back/order/src/main/kotlin/com/t4er/oralng/infrastructure/membership/MembershipReaderImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.t4er.oralng.infrastructure.membership
-
-import com.t4er.oralng.domain.membership.MembershipReader
-import org.springframework.stereotype.Component
-
-@Component
-class MembershipReaderImpl(val memberShipRepository: MemberShipRepository): MembershipReader {
-}
