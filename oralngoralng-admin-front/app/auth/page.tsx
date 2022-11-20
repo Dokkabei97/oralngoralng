1e54fb1a173134a30510e95c3e19e5f4791a9f13
Dokkabei97 <wkdrn970@naver.com>
2022-11-21 08:13:32 +0900

    feat: header에 oralngoralng 위치 수정 및 auth 페이지 ui 추가

--- oralngoralng-admin-front/app/auth/page.tsx
+++ oralngoralng-admin-front/app/auth/page.tsx
@@ -1,8 +1,34 @@
 export default function AuthPage() {
     return (
-        <div className={"bg-oralng flex justify-center"}>
-            <div>
-                HI
+        <div className={"flex justify-center"}>
+            <div className={"rounded-md border-2 border-black w-1/4 h-1/4"}>
+                <div>
+                    <div className={"flex justify-center"}>
+                        <label>
+                            Email
+                            <input
+                                type="text"
+                                className={"rounded-md border-2 w-1/2 h-1/2 m-5"}
+                            />
+                        </label>
+                    </div>
+                    <div className={"flex justify-center"}>
+                        <label>
+                            Password
+                            <input
+                                type="password"
+                                className={"rounded-md border-2 w-1/2 h-1/2 m-5"}
+                            />
+                        </label>
+                    </div>
+                    <div className={"flex justify-center"}>
+                        <button
+                            className={"rounded-md border-2 bg-oralng w-1/2 h-1/2 m-5"}
+                        >
+                            로그인
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
