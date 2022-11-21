26ca69312e2c49d06e6a1c961231af9676e66be1
Dokkabei97 <wkdrn970@naver.com>
2022-11-22 07:45:24 +0900

    feat: tailblock 도입

--- oralngoralng-admin-front/app/auth/page.tsx
+++ oralngoralng-admin-front/app/auth/page.tsx
@@ -1,35 +1,33 @@
 export default function AuthPage() {
     return (
-        <div className={"flex justify-center"}>
-            <div className={"rounded-md border-2 border-black w-1/4 h-1/4"}>
-                <div>
-                    <div className={"flex justify-center"}>
-                        <label>
-                            Email
-                            <input
-                                type="text"
-                                className={"rounded-md border-2 w-1/2 h-1/2 m-5"}
-                            />
-                        </label>
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+                <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+                    <h1 className="title-font font-medium text-3xl text-gray-900">Slow-carb next level shoindcgoitch
+                        ethical authentic, poko scenester</h1>
+                    <p className="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art
+                        gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+                </div>
+                <div
+                    className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+                    <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+                    <div className="relative mb-4">
+                        <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Email</label>
+                        <input type="text" id="full-name" name="full-name"
+                               className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
-                    <div className={"flex justify-center"}>
-                        <label>
-                            Password
-                            <input
-                                type="password"
-                                className={"rounded-md border-2 w-1/2 h-1/2 m-5"}
-                            />
-                        </label>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Password</label>
+                        <input type="email" id="email" name="email"
+                               className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
-                    <div className={"flex justify-center"}>
-                        <button
-                            className={"rounded-md border-2 bg-oralng w-1/2 h-1/2 m-5"}
-                        >
-                            로그인
-                        </button>
-                    </div>
+                    <button
+                        className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button
+                    </button>
+                    <p className="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean
+                        shorts.</p>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
