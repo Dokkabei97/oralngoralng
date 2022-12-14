f1654dc4c859f29d49d9794f14b7f621b915ea1b
Dokkabei97 <wkdrn970@naver.com>
2022-12-15 07:57:53 +0900

    feat: review db

--- oralngoralng-back/review/src/main/resources/migration/V1__init_ddl.sql
+++ oralngoralng-back/review/src/main/resources/migration/V1__init_ddl.sql
@@ -4,6 +4,9 @@ create table creator_reviews
     user_id           bigint       not null comment '리뷰 작성 크리에이터',
     title             varchar(255) not null comment '제목',
     content           text         not null comment '내용',
+    category          varchar(255) not null comment '카테고리',
+    location          varchar(255) not null comment '여행 지역',
+    theme             varchar(255) not null comment '테마',
     comment_count     int          not null comment '댓글 수',
     recommend_count   int          not null comment '추천 수',
     report_count      int          not null comment '신고 당한 수',
@@ -11,15 +14,21 @@ create table creator_reviews
     updated_at        datetime     not null comment '수정 일시'
 ) comment 'reviews' charset = utf8mb4;
 
-create table user_reviews
+create index creator_reviews_idx01 on creator_reviews (creator_review_id);
+create index creator_reviews_idx02 on creator_reviews (title);
+create index creator_reviews_idx03 on creator_reviews (category);
+create index creator_reviews_idx04 on creator_reviews (location);
+create index creator_reviews_idx05 on creator_reviews (theme);
+
+create table creator_review_comments
 (
-    user_review_id  bigint auto_increment primary key comment 'id',
-    user_id         bigint       not null comment '리뷰 작성 유저',
-    title           varchar(255) not null comment '제목',
-    content         text         not null comment '내용',
-    comment_count   int          not null comment '댓글 수',
-    recommend_count int          not null comment '추천 수',
-    report_count    int          not null comment '신고 당한 수',
-    created_at      datetime     not null comment '생성 일시',
-    updated_at      datetime     not null comment '수정 일시'
-) comment 'user_reviews' charset = utf8mb4;
+    creator_review_comment_id bigint auto_increment primary key comment 'id',
+    creator_review_id         bigint       not null comment '댓글 작성한 리뷰글',
+    user_id                   bigint       not null comment '댓글 작성자',
+    content                   varchar(300) not null comment '댓글 내용',
+    recommend_count           int          not null comment '추천 수',
+    report_count              int          not null comment '신고 당한 수',
+    created_at                datetime     not null comment '생성 일시',
+    updated_at                datetime     not null comment '수정 일시'
+)
+
