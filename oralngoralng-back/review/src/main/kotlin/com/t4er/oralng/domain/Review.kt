fe23cfe2fbc637690c380ec9a0f91001e0c455a3
Dokkabei97 <wkdrn970@naver.com>
2022-12-15 23:53:44 +0900

    feat: review sql
    - review entity

--- oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/domain/Review.kt
+++ oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/domain/Review.kt
@@ -1,4 +1,51 @@
 package com.t4er.oralng.domain
 
-class Review {
+import com.t4er.oralng.entity.AbstractEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "reviews")
+class Review(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    var id: Long,
+
+    @Column(name = "user_id")
+    var userId: Long,
+
+    @Column(name = "title")
+    var title: String,
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    var content: String,
+
+    var images: Any, //TODO Map 타입을 어캐 정의 하지?
+
+    @Column(name = "location")
+    var location: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "theme")
+    var theme: Theme,
+
+    @Column(name = "comment_count")
+    var commentCount: Int,
+
+    @Column(name = "recommend_count")
+    var recommendCount: Int,
+
+    @Column(name = "report_count")
+    var reportCount: Int,
+
+) : AbstractEntity() {
+
+    enum class Theme {
+
+    }
+
+    companion object {
+
+    }
 }
--- oralngoralng-back/review/src/main/resources/migration/V1__init_ddl.sql
+++ oralngoralng-back/review/src/main/resources/migration/V1__init_ddl.sql
@@ -1,34 +1,76 @@
-create table creator_reviews
+create table reviews
 (
-    creator_review_id bigint auto_increment primary key comment 'id',
-    user_id           bigint       not null comment '리뷰 작성 크리에이터',
-    title             varchar(255) not null comment '제목',
-    content           text         not null comment '내용',
-    category          varchar(255) not null comment '카테고리',
-    location          varchar(255) not null comment '여행 지역',
-    theme             varchar(255) not null comment '테마',
-    comment_count     int          not null comment '댓글 수',
+    review_id       bigint auto_increment primary key comment 'id',
+    user_id         bigint       not null comment '리뷰 작성 크리에이터 id',
+    title           varchar(255) not null comment '제목',
+    content         text         not null comment '내용',
+    images          json         not null comment '이미지',
+    location        varchar(255) not null comment '여행 지역',
+    theme           varchar(255) not null comment '테마',
+    comment_count   int          not null comment '댓글 수',
+    recommend_count int          not null comment '추천 수',
+    report_count    int          not null comment '신고 당한 수',
+    created_at      datetime     not null comment '생성 일시',
+    updated_at      datetime     not null comment '수정 일시'
+) comment 'reviews' charset = utf8mb4;
+
+create index reviews_idx01 on reviews (review_id);
+create index reviews_idx02 on reviews (title);
+create index reviews_idx04 on reviews (location);
+create index reviews_idx05 on reviews (theme);
+
+create table review_comments
+(
+    review_comment_id bigint auto_increment primary key comment 'id',
+    review_id         bigint       not null comment '댓글 작성한 리뷰글 id',
+    user_id           bigint       not null comment '댓글 작성한 유저 id',
+    content           varchar(300) not null comment '댓글 내용',
     recommend_count   int          not null comment '추천 수',
     report_count      int          not null comment '신고 당한 수',
     created_at        datetime     not null comment '생성 일시',
     updated_at        datetime     not null comment '수정 일시'
-) comment 'reviews' charset = utf8mb4;
+) comment 'review_comments' charset = utf8mb4;
 
-create index creator_reviews_idx01 on creator_reviews (creator_review_id);
-create index creator_reviews_idx02 on creator_reviews (title);
-create index creator_reviews_idx03 on creator_reviews (category);
-create index creator_reviews_idx04 on creator_reviews (location);
-create index creator_reviews_idx05 on creator_reviews (theme);
+create index review_comments_idx01 on review_comments (review_id);
 
-create table creator_review_comments
+create table recommend_reviews
 (
-    creator_review_comment_id bigint auto_increment primary key comment 'id',
-    creator_review_id         bigint       not null comment '댓글 작성한 리뷰글',
-    user_id                   bigint       not null comment '댓글 작성자',
-    content                   varchar(300) not null comment '댓글 내용',
-    recommend_count           int          not null comment '추천 수',
-    report_count              int          not null comment '신고 당한 수',
-    created_at                datetime     not null comment '생성 일시',
-    updated_at                datetime     not null comment '수정 일시'
-)
+    recommend_review_id bigint auto_increment primary key comment 'id',
+    user_id             bigint   not null comment '추천 한 유저 id',
+    review_id           bigint   not null comment '추천 한 리뷰 id',
+    created_at          datetime not null comment '생성 일시',
+    updated_at          datetime not null comment '수정 일시'
+) comment 'recommend_reviews' charset = utf8mb4;
 
+create index recommend_reviews_idx01 on recommend_reviews (recommend_review_id);
+create index recommend_reviews_idx02 on recommend_reviews (user_id);
+create index recommend_reviews_idx03 on recommend_reviews (review_id);
+
+create table report_reviews
+(
+    report_review_id bigint auto_increment primary key comment 'id',
+    user_id          bigint       not null comment '신고 한 유저 id',
+    review_id        bigint       not null comment '신고 한 리뷰 id',
+    reason           varchar(300) not null comment '신고 사유',
+    created_at       datetime     not null comment '생성 일시',
+    updated_at       datetime     not null comment '수정 일시'
+) comment 'report_reviews' charset = utf8mb4;
+
+create table recommend_comments
+(
+    recommend_comment_id bigint auto_increment primary key comment 'id',
+    user_id              bigint   not null comment '추천 한 유저 id',
+    comment_id           bigint   not null comment '추천 한 댓글 id',
+    created_at           datetime not null comment '생성 일시',
+    updated_at           datetime not null comment '수정 일시'
+) comment 'recommend_comments' charset = utf8mb4;
+
+create table report_comments
+(
+    report_comment_id bigint auto_increment primary key comment 'id',
+    user_id           bigint       not null comment '신고 한 유저 id',
+    comment_id        bigint       not null comment '신고 한 댓글 id',
+    reason            varchar(300) not null comment '신고 사유',
+    created_at        datetime     not null comment '생성 일시',
+    updated_at        datetime     not null comment '수정 일시'
+) comment 'report_comments' charset = utf8mb4;
