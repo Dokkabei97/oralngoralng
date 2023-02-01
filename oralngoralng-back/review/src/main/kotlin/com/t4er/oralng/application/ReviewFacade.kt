3e026da6a4fe764009b4190a70a2a94e84e03423
Dokkabei97 <wkdrn970@naver.com>
2023-02-02 07:45:58 +0900

    feat: controller

--- oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/application/ReviewFacade.kt
+++ oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/application/ReviewFacade.kt
@@ -1,8 +1,18 @@
 package com.t4er.oralng.application
 
+import com.t4er.oralng.domain.ReviewCommand.*
+import com.t4er.oralng.domain.ReviewInfo
 import com.t4er.oralng.domain.ReviewService
 import org.springframework.stereotype.Service
 
 @Service
 class ReviewFacade(val reviewService: ReviewService) {
+
+    fun createReview(command: CreateReviewRequest): ReviewInfo {
+        return reviewService.create(command)
+    }
+
+    fun updateReview(command: UpdateReviewRequest): ReviewInfo {
+        return reviewService.update(command)
+    }
 }
