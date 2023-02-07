96eb7f0c5a36c7ce8e218129f83099468f896362
Dokkabei97 <wkdrn970@naver.com>
2023-02-08 07:47:15 +0900

    feat: kafka, redis 설정

--- oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/config/KafkaConfig.kt
+++ oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/config/KafkaConfig.kt
@@ -1,4 +1,12 @@
 package com.t4er.oralng.config
 
-class KafkaConfig {
+import org.springframework.kafka.core.KafkaTemplate
+
+class KafkaConfig(val template: KafkaTemplate<Int, String>) {
+
+    fun send() = template.send(TOPIC_NAME, "kafka test")
+
+    companion object {
+        const val TOPIC_NAME = "review"
+    }
 }
--- oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/config/RedisConfig.kt
+++ oralngoralng-back/review/src/main/kotlin/com/t4er/oralng/config/RedisConfig.kt
@@ -1,4 +1,18 @@
 package com.t4er.oralng.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisSentinelConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
 class RedisConfig {
+
+    @Bean
+    fun lettuceConnectionFactory(): RedisConnectionFactory {
+        val sentinelConfig: RedisSentinelConfiguration = RedisSentinelConfiguration()
+            .master("master")
+            .sentinel("127.0.0.1", 26379)
+            .sentinel("127.0.0.1", 26380)
+        return LettuceConnectionFactory(sentinelConfig)
+    }
 }
