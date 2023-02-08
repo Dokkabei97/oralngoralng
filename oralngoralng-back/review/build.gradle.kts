1affa85d97a3b60f1e7a41b16e4dad78d14cf623
Dokkabei97 <wkdrn970@naver.com>
2023-02-09 07:23:08 +0900

    feat: elk 버전 수정

--- oralngoralng-back/review/build.gradle.kts
+++ oralngoralng-back/review/build.gradle.kts
@@ -8,7 +8,8 @@ repositories {
 
 dependencies {
     implementation(project(":common"))
-    implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+    // spring data elasticsearch 는 버전에 따라 지원 하는 elasticsearch 버전이 다르니 버전을 고정
+    implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch:5.0.1")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.1.1")
